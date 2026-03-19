66a44111a06404ded14209a3d65a7195ffc954bf
m4rqu1t <marcotab422@gmail.com>
2026-03-19 13:18:32 +0000

    Leitura do tempo total da simulacao

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct tarefa{
+    char nome[5];
+    int periodo;
+    int cpuBurst;
+}tarefa;
+
 int main( int argc, char *argv[]){
 
     if(argc != 2){
@@ -15,7 +21,20 @@ int main( int argc, char *argv[]){
 
         printf("ERRO: ARQUIVO NAO EXISTE OU NAO PODE SER ABERTO\n");
         return 1;
+
     }
+
+    int tempoTotal;
+    if(fscanf(arquivo, "%d", &tempoTotal) != 1){
+
+        printf("ERRO AO LER O TEMPO TOTAL NO INICIO DO ARQUIVO\n");
+        fclose(arquivo);
+        return 1;
+
+    }
+
+    printf("TEMPO DEFINIDO : %d\n", tempoTotal);
+
 
 
 
